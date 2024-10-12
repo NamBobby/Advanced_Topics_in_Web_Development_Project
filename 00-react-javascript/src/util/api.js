0449fcdf1aa4660acae9356758e939b46f661c0a
NamBobby <lethpnam27@gmail.com>
2024-10-12 19:14:00 +0700

    complete login api

--- 00-react-javascript/src/util/api.js
+++ 00-react-javascript/src/util/api.js
@@ -8,6 +8,14 @@ const createUserApi = (name, email, password) => {
     return axios.post(URL_API, data)
 }
 
+const LoginApi = (email, password) => {
+    const URL_API = "/v1/api/login"
+    const data = {
+        email, password
+    }
+    return axios.post(URL_API, data)
+}
+
 export {
-    createUserApi
+    createUserApi,LoginApi
 }
--- 01-nodejs-javascript/src/services/userService.js
+++ 01-nodejs-javascript/src/services/userService.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const User = require("../models/user");
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const createUserService = async (name, email, password) => {
     try {
@@ -27,7 +29,6 @@ const loginService = async (email, password) => {
         const user = await User.findOne({ email: email});
         if (user){
             //compare password
-            console.log(">>> check user: ", user)
             const isMatchPassword = await bcrypt.compare(password, user.password)
             if(!isMatchPassword) {
                 return {
@@ -36,7 +37,26 @@ const loginService = async (email, password) => {
                 }
             }else {
                 //create an access token
-                return "create an access token"; 
+                const payload = {
+                    email: user.email,
+                    name: user.name
+                }
+
+                const access_token = jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: process.env.JWT_EXPIRE
+                    }
+                 )
+                return {
+                    EC: 0,
+                    access_token,
+                    user: {
+                        email: user.email,
+                        name: user.name
+                    }
+                }; 
             }
         }else {
             return {
