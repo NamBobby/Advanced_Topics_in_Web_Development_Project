2a2f6a7af9ff87aa497dac33fdfaf3b2d4469414
Lê Thanh Phương Nam <lethpnam27@gmail.com>
2024-12-10 15:22:20 +0700

    userinfo 1.0

--- 00-react-javascript/src/containers/profileInfoPage/userInfo.jsx
+++ 00-react-javascript/src/containers/profileInfoPage/userInfo.jsx
@@ -1,13 +1,28 @@
-import React, { useContext } from 'react';
-
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import Song from "../../components/song";
+import Album from "../../components/album";
+import "../../assets/styles/userInfo.css";
 
 const UserInfo = () => {
-  const navigate = useNavigate();
   return (
-    <div>
-      <legend>USER PAGE</legend>
-      <legend>user Active...</legend>
+    
+      <div className="user-info-overlay">
+        <div className="background-container">
+          <div className="user-detail">
+            <p className="user-role">Artist</p>
+            <h3 className="user-name">John Doe</h3>
+          </div>
+        <div className="userinfo-content">
+          <div className="title-header">
+            <h2 className="title">Music</h2>
+          </div>
+          <Song />
+          <div className="title-header">
+            <h2 className="title">Albums</h2>
+          </div>
+          <Album />
+        </div>
+      </div>
     </div>
   );
 };
