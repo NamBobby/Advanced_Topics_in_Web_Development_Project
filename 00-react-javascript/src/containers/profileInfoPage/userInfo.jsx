d34066bf36b2b3b3048945f2aeb5bed67d02cad7
NamBobby <lethpnam27@gmail.com>
2024-12-11 22:29:44 +0700

    userInfo done
    albumPopup
    
    Revert "userinfo 1.0"
    
    This reverts commit 2a2f6a7af9ff87aa497dac33fdfaf3b2d4469414.
    
    Revert "albumPopup"
    
    This reverts commit 7df834181d8c58dd61589695e1ad915a7bf50757.
    
    Revert "userInfo done"
    
    This reverts commit 806b4b38ac01a8c2c626b17e116a45696d87906f.

--- 00-react-javascript/src/containers/profileInfoPage/userInfo.jsx
+++ 00-react-javascript/src/containers/profileInfoPage/userInfo.jsx
@@ -1,28 +1,13 @@
-import React from "react";
-import Song from "../../components/song";
-import Album from "../../components/album";
-import "../../assets/styles/userInfo.css";
+import React, { useContext } from 'react';
+
+import { Link, useNavigate } from 'react-router-dom';
 
 const UserInfo = () => {
+  const navigate = useNavigate();
   return (
-    
-      <div className="user-info-overlay">
-        <div className="background-container">
-          <div className="user-detail">
-            <p className="user-role">Artist</p>
-            <h3 className="user-name">John Doe</h3>
-          </div>
-        <div className="userinfo-content">
-          <div className="title-header">
-            <h2 className="title">Music</h2>
-          </div>
-          <Song />
-          <div className="title-header">
-            <h2 className="title">Albums</h2>
-          </div>
-          <Album />
-        </div>
-      </div>
+    <div>
+      <legend>USER PAGE</legend>
+      <legend>user Active...</legend>
     </div>
   );
 };
