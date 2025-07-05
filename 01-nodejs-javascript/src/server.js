e031146fe8d2650f10530cbc1298ffcccea85603
Lê Thanh Phương Nam <lethpnam27@gmail.com>
2025-07-05 18:29:38 +0700

    Update server.js

--- 01-nodejs-javascript/src/server.js
+++ 01-nodejs-javascript/src/server.js
@@ -14,12 +14,14 @@ const app = express();
 const port = process.env.PORT || 8888;
 
 // CORS setup
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 
 // Body parsing
 app.use(express.json());
